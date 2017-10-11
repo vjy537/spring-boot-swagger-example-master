db60ff02bb62f970c41788d50626f4f49c72a385
vjy537 <vijay.chary37@gmail.com>
2017-10-11 12:06:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Boot integration with Swagger 2 UI
+#https://www.youtube.com/watch?v=HHyjWc0ASl8
+
+`http://localhost:8088/swagger-ui.html` - Shows the list of Endpoints in the current RESTful webservice.
---
+++ src/main/java/com/techprimers/springboot/swaggerexample/SwaggerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.techprimers.springboot.swaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExampleApplication {
+//https://www.youtube.com/watch?v=HHyjWc0ASl8
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerExampleApplication.class, args);
+	}
+}
